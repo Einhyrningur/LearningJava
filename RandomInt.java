ed464a8c8b23538d964bb4bf6acd2acac52281b0
Sindri Frostason <sindri.frostason@gmail.com>
2017-04-28 15:42:08 +0200

    Curly braces should be just so

--- RandomInt.java
+++ RandomInt.java
@@ -1,7 +1,6 @@
-public class RandomInt
-{
-  public static void main(String[] args)
-  {
+public class RandomInt {
+  public static void main(String[] args) {
+
     int N = Integer.parseInt(args[0]);
     double r = Math.random(); // uniform between 0 and 1
     int n = (int) (r * N); // uniform between 0 and N-1
