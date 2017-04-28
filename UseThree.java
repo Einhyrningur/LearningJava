8c51da189c688816a183177f62250c21b75a57e3
Andrea Rakel Sigurðardóttir <andrearakel@Andreas-MacBook-Pro-2.local>
2017-04-28 15:34:45 +0200

    First Commit

---
+++ UseThree.java
@@ -0,0 +1,17 @@
+public class UseThree
+{
+  public static void main(String[] args)
+  {
+    /*System.out.print("Hi, ");
+    System.out.print(args [2]);
+    System.out.print(", ");
+    System.out.print(args[1]);
+    System.out.print(" and ");
+    System.out.print(args[0]);
+    System.out.println(".");*/
+
+    System.out.print("Hi, " + args[2] + ", " + args[1] + " and " + args[0]);
+    System.out.println();
+  }
+
+}
